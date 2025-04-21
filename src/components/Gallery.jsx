b4526a5ae7da8b341dada0ed2db42aceb9064e71
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-04-20 20:36:31 -0400

    Reset View - Enabled reset button functionality

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,26 +10,19 @@ const filteredTours = destination === 'All Options' ? tours : tours?.filter(tour
 if (loading) return <h1>Loading</h1>
 if (error) return <h1>{error}</h1>
 
-/*if(filteredTours?.length === 0) {
+// refreshes the page to restore tours if the filteredTours array is empty 
+if(filteredTours?.length === 0) {
     return (
     <div>
         <h2>No tours left. Refresh to reload</h2>
-        <button onClick={(() => console.log("test"))}>Refresh</button>
+        <button onClick={(() => window.location.reload(false))}>Refresh</button>
     </div>
     )
-} */
+} 
 
 // renders TourCard and passes it meaningful props, enables filteredTours to change what is displayed
 return (
     <div id = "gallery">
-       
-        <button onClick={() => {
-        console.log("Current destination:", destination);
-        console.log("Tours data:", tours);
-        console.log("Filtered tours:", filteredTours);
-        }}>TESTING BUTTON</button>
-
-
         {filteredTours?.map(tour => ( 
             <TourCard
             key={tour.id}
